cc7a0f398f9897a1f73fa99af6083c1d01997602
stefann <stefann.sasori@gmail.com>
2021-02-03 07:45:36 +0000

    fixed reset link

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -50,7 +50,7 @@ class ResetPasswordController extends AbstractController
 
 
                 $content = 'Bonjour ' .$user->getFirstname(). "<br/></br> Vous avez demandé à réinitialiser votre mot de passe sur le site Recherche Edification et Développement<br/><br/>";
-                $content .= "Merci de bien vouloir cliquer sur le lien suivant pour <a href'".$url."'> mettre à jour votre mot de passe.</a>";
+                $content .= 'Merci de bien vouloir cliquer sur le lien suivant pour <a href="'.$url.'"> mettre à jour votre mot de passe.</a>';
 
                 $mail = new Mail();
                 $mail->send($user->getEmail(), $user->getFirstname().' '.$user->getLastname(), 'Réinitialiser votre mot de passe', $content);
